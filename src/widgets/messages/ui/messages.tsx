69f53a74a0c3aea350e854f6a5543edc1bcc8a60
hubtataevsh <tataev.shokan@bcchub.kz>
2024-06-17 09:40:54 +0500

    [feature]: add loader to gpt chat

--- src/widgets/messages/ui/messages.tsx
+++ src/widgets/messages/ui/messages.tsx
@@ -3,10 +3,15 @@ import { Message } from "@/shared/ui"
 
 export const Messages = () => {
 	const { userInfo } = useAppSelector((state) => state.userReducer)
-	const { messages, chatId } = useAppSelector((state) => state.chatsReducer)
+	const { messages, chatId, isGPTLoading } = useAppSelector((state) => state.chatsReducer)
 	useChatMessages(chatId)
 	return (
 		<div className={"h-96 grow flex flex-col gap-6 bg-[#FBFBFB] overflow-y-scroll p-10"}>
+			{isGPTLoading && (
+				<div className={"w-full h-full flex justify-center items-center"}>
+					<div className="w-12 h-12 rounded-full animate-spin border-8 border-dashed border-[#9969FF] border-t-transparent" />
+				</div>
+			)}
 			{messages.map((message, index) => (
 				<Message
 					key={index}
